7b4fb2df549c1a6eea065f2e2f4c9de239260de7
imran3737 <hisence1@gmail.com>
2020-05-10 15:00:21 +0500

    my first repository as counter

---
+++ counter_crate/target/package/counter_crate-0.1.0/target/debug/deps/counter_crate-56e1fc40b8dc2ac0.d
@@ -0,0 +1,7 @@
+C:\Users\Panasonic CF-31\Desktop\Demo repository\my-first-welcome-repo\counter_crate\target\package\counter_crate-0.1.0\target\debug\deps\counter_crate-56e1fc40b8dc2ac0.rmeta: src\lib.rs
+
+C:\Users\Panasonic CF-31\Desktop\Demo repository\my-first-welcome-repo\counter_crate\target\package\counter_crate-0.1.0\target\debug\deps\libcounter_crate-56e1fc40b8dc2ac0.rlib: src\lib.rs
+
+C:\Users\Panasonic CF-31\Desktop\Demo repository\my-first-welcome-repo\counter_crate\target\package\counter_crate-0.1.0\target\debug\deps\counter_crate-56e1fc40b8dc2ac0.d: src\lib.rs
+
+src\lib.rs:
---
+++ counter_crate/target/package/counter_crate-0.1.0/target/debug/libcounter_crate.d
@@ -0,0 +1,1 @@
+C:\Users\Panasonic\ CF-31\Desktop\Demo\ repository\my-first-welcome-repo\counter_crate\target\package\counter_crate-0.1.0\target\debug\libcounter_crate.rlib: C:\Users\Panasonic\ CF-31\Desktop\Demo\ repository\my-first-welcome-repo\counter_crate\target\package\counter_crate-0.1.0\src\lib.rs
